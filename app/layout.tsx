4963aefcc7d2bfe9820b21bec8a6e7e4e2092094
Håvar Rysstad <hrysstad@gmail.com>
2025-10-30 13:55:00 +0100

    Add Google Analytics (G-KCVJGV33QE) and Google Tag Manager (GTM-PSC7HGP8)

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 
 import { Suspense } from "react"
+import Script from "next/script"
 import "./globals.css"
 
 import { Roboto, Geist as V0_Font_Geist, Geist_Mono as V0_Font_Geist_Mono, Source_Serif_4 as V0_Font_Source_Serif_4 } from 'next/font/google'
@@ -31,7 +32,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="no">
+      <head>
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{__html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PSC7HGP8');
+        `}} />
+
+        {/* Google Analytics (GA4) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-KCVJGV33QE"></script>
+        <script dangerouslySetInnerHTML={{__html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-KCVJGV33QE');
+        `}} />
+      </head>
       <body className={`${roboto.variable} font-sans`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript dangerouslySetInnerHTML={{__html: `
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PSC7HGP8"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe>
+        `}} />
+
         <Suspense fallback={null}>{children}</Suspense>
       </body>
     </html>
